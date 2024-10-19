c142ae8971543f7d1825ff1b32e8b1949fe18b16
Arroziqi <ahmadarroziqi@gmail.com>
2024-10-19 18:45:36 +0700

    setup zod validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { PrismaService } from './common/prisma.service';
+import { ValidationService } from './common/validation.service';
 @Module({
   imports: [CommonModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
 })
 export class AppModule {}
--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,8 +1,9 @@
-import { Inject, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 
+@Injectable()
 export class PrismaService
   extends PrismaClient<Prisma.PrismaClientOptions, string>
   implements OnModuleInit
